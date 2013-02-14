a5b9e5f1e44cc7f8d861c895c425577a8632cd54
Rex Sheng <shengning@gmail.com>
2013-02-14 15:09:19 +0800

    add accuracy

--- Versions/A/Headers/ZXingQRCodeScanViewController.h
+++ Versions/A/Headers/ZXingQRCodeScanViewController.h
@@ -7,8 +7,8 @@
 
 #import <UIKit/UIKit.h>
 #import "ZXingWidgetController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@class CLLocation;
 @class OverlayView;
 
 @interface ZXingQRCodeScanViewController : ZXingWidgetController
@@ -16,6 +16,7 @@
 @property (nonatomic, retain) OverlayView *overlayView;
 @property (nonatomic, retain) CLLocation *location;
 @property (nonatomic) BOOL scanWithLocation;
+@property (nonatomic) CLLocationAccuracy desiredAccuracy;
 
 - (void)finishScan:(NSString *)scanResult;
 - (void)scanDidCancel;
